d65316a4fdd97700c9eac03a3726ac13e896fcbe
hrbtk <viktor.beskorovainyi@gmail.com>
2025-01-23 11:56:58 +0100

    updated gnome setup file

--- 7_gnome.sh
+++ 7_gnome.sh
@@ -5,24 +5,23 @@ pipx install gnome-extensions-cli --system-site-packages
 pipx ensurepath
 
 # Install gnome extensions
-gext install user-theme@gnome-shell-extensions.gcampax.github.com
-gext install dash-to-dock@micxgx.gmail.com
-gext install clipboard-indicator@tudmotu.com
-gext install caffeine@patapon.info
-gext install blur-my-shell@aunetx
-gext install tilingshell@ferrarodomenico.com
-gext install quick-settings-audio-panel@rayzeq.github.io
-gext install gsconnect@andyholmes.github.io
 gext install Airpod-Battery-Monitor@maniacx.github.com
 gext install appindicatorsupport@rgcjonas.gmail.com
 gext install Bluetooth-Battery-Meter@maniacx.github.com
+gext install blur-my-shell@aunetx
+gext install caffeine@patapon.info
+gext install clipboard-indicator@tudmotu.com
+gext install dash-to-dock@micxgx.gmail.com
+gext install display-brightness-ddcutil@themightydeity.github.com
 gext install emoji-copy@felipeftn
-gext install transparent-top-bar@ftpix.com
+gext install gnome-fuzzy-app-search@gnome-shell-extensions.Czarlie.gitlab.com
+gext install gsconnect@andyholmes.github.io
+gext install media-progress@krypion17
+gext install privacy-indicators-accent-color@sopht.li
 gext install rounded-window-corners@fxgn
-gext install extension-list@tu.berry
-gext install fullscreen-avoider@noobsai.github.com
-gext install tophat@fflewddur.github.io
-# gext install forge@jmmaranan.com
+gext install status-area-horizontal-spacing@mathematical.coffee.gmail.com
+gext install tilingshell@ferrarodomenico.com
+gext install user-theme@gnome-shell-extensions.gcampax.github.com
 
 # Load keybindings to dconf
 dconf load /org/gnome/desktop/wm/keybindings/ < ~/dotfiles/fedora_after_install/donf_dumps/wmkbdump.dconf
